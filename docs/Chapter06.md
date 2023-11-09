8bbdaf58288cc6200164d666803ffb11e52c49ff
Marley <marley.nodejs@gmail.com>
2023-11-09 21:43:50 +0300

    06. Deleting Issues | 01. Adding a Delete Button

---
+++ docs/Chapter06.md
@@ -0,0 +1,24 @@
+# [Laith Harb] The Next.js 13 Bootcamp - The Complete Developer Guide [ENG, 2023]
+
+<br/>
+
+## [06. Deleting Issues](https://github.com/webmakaka/Next.js-Projects-Build-an-Issue-Tracker/pull/5)
+
+<br/>
+
+### 01. Adding a Delete Button
+
+<br/>
+
+![Application](/img/pic-ch06-img01.png?raw=true)
+
+<br/>
+
+---
+
+<br/>
+
+**Marley**
+
+Any questions in english: <a href="https://jsdev.org/chat/">Telegram Chat</a>  
+Любые вопросы на русском: <a href="https://jsdev.ru/chat/">Телеграм чат</a>
--- proj/src/app/layout.tsx
+++ proj/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import NavBar from '@/app/NavBar';
-import { Theme } from '@radix-ui/themes';
+import { Container, Theme } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -23,7 +23,9 @@ export default function RootLayout({
       <body className={inter.variable}>
         <Theme appearance="light" accentColor="violet">
           <NavBar />
-          <main className="p-5">{children}</main>
+          <main className="p-5">
+            <Container>{children}</Container>
+          </main>
         </Theme>
       </body>
     </html>
